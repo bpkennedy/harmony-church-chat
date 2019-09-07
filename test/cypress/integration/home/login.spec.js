7b9f1d27511fcb0ccd622c74a72f44ba9d2936b9
Brian Kennedy <bpkennedy@gmail.com>
2019-09-06 23:43:16 -0500

    Add cypress e2e testing and CI build step for testing

---
+++ test/cypress/integration/home/login.spec.js
@@ -0,0 +1,40 @@
+import * as ctx from  '../../../../quasar.conf.js'
+
+describe('Login', () => {
+  beforeEach(() => {
+    cy.visit('/')
+    indexedDB.deleteDatabase('firebaseLocalStorageDb')
+  })
+
+  it('should go to login page', () => {
+    cy.get('body').contains('Login').click()
+    cy.contains('Your password')
+    cy.contains('Your email')
+  })
+
+  it('should login as test account', () => {
+    cy.get('body').contains('Login').click()
+    cy.getLabel('Your email *').click().type('test@email.com')
+    cy.getLabel('Your password *').click().type('pass123')
+    cy.get('body').contains('Submit').click()
+    cy.get('body').contains('Login').should('not.exist')
+  })
+
+  it('should display error popover if failed login', () => {
+    cy.get('body').contains('Login').click()
+    cy.getLabel('Your email *').click().type('badEmail@email.com')
+    cy.getLabel('Your password *').click().type('pass123')
+    cy.get('body').contains('Submit').click()
+    cy.get('body').contains('There is no user record corresponding to this identifier.').should('exist')
+  })
+
+  it('should clear form if hit reset', () => {
+    cy.get('body').contains('Login').click()
+    cy.getLabel('Your email *').click().type('test@email.com')
+    cy.getLabel('Your password *').click().type('pass123')
+    cy.contains('Reset').click()
+    cy.contains('test@email.com').should('not.exist')
+    cy.contains('pass123').should('not.exist')
+  })
+
+})
